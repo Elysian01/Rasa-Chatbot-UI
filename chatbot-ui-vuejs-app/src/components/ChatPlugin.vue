ae04dff743abac67e75f93a365a540913e5d9c54
sangeetmore <san.mor.rt20@dypatil.edu>
2023-04-21 23:10:19 +0530

    :sparkles: added new Rasa Chatbot Plugin

---
+++ chatbot-ui-vuejs-app/src/components/ChatPlugin.vue
@@ -0,0 +1,18 @@
+<template>
+    <div id="rasa-chat-widget" :data-websocket-url="connUrl" :data-avatar-url="avatarUrl"></div>
+</template>
+
+<script>
+export default{
+    name: 'ChatPlugin',
+    data:()=>({
+        connUrl: "http://localhost:5005",
+        avatarUrl: "https://st3.depositphotos.com/8950810/17657/v/600/depositphotos_176577870-stock-illustration-cute-smiling-funny-robot-chat.jpg",
+    }),
+    mounted() {
+      let recaptchaScript = document.createElement('script')
+      recaptchaScript.setAttribute('src', 'https://unpkg.com/@rasahq/rasa-chat')
+      document.head.appendChild(recaptchaScript)
+    },
+}
+</script>
